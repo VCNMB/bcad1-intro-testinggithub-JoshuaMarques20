5fc647f812b8010945ac58dc01b26763b29addc2
JoshuaMarques20 <ST10438843@vcconnect.edu.za>
2025-03-11 10:39:05 +0200

    added newClass

--- PrjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
+++ PrjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
@@ -11,6 +11,10 @@ package com.mycompany.prjclasses;
 public class PrjClasses {
 
     public static void main(String[] args) {
-        System.out.println("Hello World! I just did GitHub and did your mom in a good way");
+       newClass nc = new newClass(); // creating an object of newClass
+       
+       nc.helloClass(); // have to use variable name nc to communicate with the calls
+       
+        System.out.println(nc.strSecondClassVar); // output varible from 2nd class
     }
 }
